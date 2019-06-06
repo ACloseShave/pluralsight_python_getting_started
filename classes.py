f2eee3aaed07c03c4656abf662acc08ffab3112e
acloseshave <ryan.shaver@live.com>
2019-06-06 12:50:32 -0500

    ~ Updated classes.py with constructor methods, going to add instance and class attributes to simplify in the next push.

--- classes.py
+++ classes.py
@@ -3,12 +3,15 @@ students = []
 
 class Student:
     def __init__(self, name, student_id=332):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
-
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
 
     def __str__(self):
-        return "Student"
+        return "Student " + self.name
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
 
 test_student = Student("Test")
 print(test_student)
