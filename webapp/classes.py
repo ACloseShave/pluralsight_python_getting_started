3ffc8a3d7b2a0d88b1f743e4471c8a651ab5b0a4
acloseshave <ryan.shaver@live.com>
2019-06-07 09:28:37 -0500

    Finishing webapp build

--- webapp/classes.py
+++ webapp/classes.py
@@ -3,31 +3,34 @@ students = []
 
 class Student:
 
-    school_name = "Springfield Elementary"
+	school_name = "Springfield Elementary"
 
-    def __init__(self, name, student_id=332):
-        self.name = name
-        self.student_id = student_id
-        students.append(self)
+	def __init__(self, name, student_id=332):
+		self.name = name
+		self.student_id = student_id
+		students.append(self)
 
-    def __str__(self):
-        return "Student " + self.name
+	def __str__(self):
+		return "Student " + self.name
 
-    def get_name_capitalize(self):
-        return self.name.capitalize()
+	def get_name_capitalize(self):
+		return self.name.capitalize()
 
-    def get_school_name(self):
-        return self.school_name
+	def get_school_name(self):
+		return self.school_name
 
 
 class HighSchoolStudent(Student):
 
-    school_name = "Springfield High School"
+	school_name = "Springfield High School"
 
-    def get_school_name(self):
-        return "This is a high school student"
+	def get_school_name(self):
+		return "This is a High School student"
 
-    def get_name_capitalize(self):
-        original_value = super().get_name_capitalize()
-        return original_value + "-HS"
+	def get_name_capitalize(self):
+		original_value = super().get_name_capitalize()
+		return original_value + "-HS"
 
+
+james = HighSchoolStudent("james")
+print(james.get_name_capitalize())
--- webapp/functions.py
+++ webapp/functions.py
@@ -2,29 +2,48 @@ students = []
 
 
 def get_students_titlecase():
-    students_titlecase = []
-    for student in students:
-        students_titlecase = student.title()
-    return students_titlecase
+	students_titlecase = []
+	for student in students:
+		students_titlecase.append(student["name"].title())
+	return students_titlecase
 
 
 def print_students_titlecase():
-    students_titlecase = get_students_titlecase()
-    print(students_titlecase)
+	students_titlecase = get_students_titlecase()
+	print(students_titlecase)
 
 
 def add_student(name, student_id=332):
-    student = {"name": name, "student_id": student_id }
-    students.append(student)
+	student = {"name": name, "student_id": student_id}
+	students.append(student)
 
 
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs)
+def save_file(student):
+	try:
+		f = open("students.txt", "a")
+		f.write(student + "\n")
+		f.close()
+	except Exception:
+		print("Could not save file")
 
 
-student_list = get_students_titlecase()
+def read_file():
+	try:
+		f = open("students.txt", "r")
+		for student in f.readlines():
+			add_student(student)
+		f.close()
+	except Exception:
+		print("Could not read file")
 
-add_student(name="Mark", student_id=15)
 
-var_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
+read_file()
+print_students_titlecase()
+
+student_name = input("Enter student name: ")
+student_id = input("Enter student ID: ")
+
+add_student(student_name, student_id)
+save_file(student_name)
+
+
